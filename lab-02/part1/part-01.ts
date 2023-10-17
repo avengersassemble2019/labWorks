47d5e55c2e97c2e8bd271faf5a7016a09aadeda8
basil amso <124886339+HiItsBasilio@users.noreply.github.com>
2023-10-17 09:27:44 +0800

    added gitignore

--- lab-02/part1/part-01.ts
+++ lab-02/part1/part-01.ts
@@ -1,11 +1,11 @@
 // a function in node called require that loads modules
 let http = require('http');
-import{ IncomingMessage, ServerResponse } from 'node:http';
+import { IncomingMessage, ServerResponse } from 'node:http';
 
-function handleRequest(request:IncomingMessage, response:ServerResponse) {
+function handleRequest(request: IncomingMessage, response: ServerResponse) {
   let url = request.url;
   let method = request.method;
-  
+
   console.log('Debugging -- url is', url, 'while method is', method);
 
   // asynchronous operation fetching data from a database
@@ -13,7 +13,7 @@ function handleRequest(request:IncomingMessage, response:ServerResponse) {
     response.writeHead(200, { 'Content-Type': 'text/plain' }); // response headers
     response.end('Basilio sent me here to tell you hi' + url); // response will be in the body
   }, 1000);
-   // 200 tells the browser the response is successful, memorize the common ones: 200, 401, 403, 404, 500 important
+  // 200 tells the browser the response is successful, memorize the common ones: 200, 401, 403, 404, 500 important
 }
 
 const server = http.createServer(handleRequest);
@@ -24,3 +24,4 @@ const port = 3000;
 server.listen(port, hostname, () => {
   console.log(`Server started at http://${hostname}:${port}/`);
 });
+
